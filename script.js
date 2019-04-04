760ce5c6701844626e3806d92319dcc59c5a72cc
jarbun <jarbun306@gmail.com>
2019-04-04 15:43:59 +0530

    Added logic to obtain current date

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ new Vue({
       });
       this.nextId++;
       this.newTask='';
+    },
+    getDate: function () {
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      let d = new Date();
+      return `${weekdays[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]}`;
     }
   }
 })
